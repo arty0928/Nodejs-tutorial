1c7431e405561789fd8740bfebc1e9c2bd2fa2f4
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-01-25 09:49:05 +0900

    "4-10"

--- 10-file/app.js
+++ 10-file/app.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
 //3
-//rename(.....,callback(error,data)) 비동기
+//rename(.....,callback(error,data)) blocking
 //try {renameSync(....)} catch(e) {}
-//promise.rename().then().catch(e)
+//promise.rename().then().catch(0)
 
 
 //동기적 파일 이름 변경
 try{
-    fs.renameSync('./text.txt','./file-new.txt');
+    fs.renameSync('./text.txt','./text-new.txt');
 }
 catch(error){
     console.error(error);
@@ -16,8 +16,12 @@ catch(error){
 
 //비동기적 파일 이름 변경
 fs.rename('./text-new.txt', './text.txt', (error) =>{
-    console.log(error);
+    console.log(error); //error가 발생하지 않으면 null 출력
 });
 
 console.log('hello');
 
+fs.promises
+.rename('./text.txt','./text2-new.txt')
+.then(()=>console.log('done'))
+.catch(console.error);
